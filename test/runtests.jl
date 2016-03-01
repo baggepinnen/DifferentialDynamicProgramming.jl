09d5671c507b2311f253ab5f256ec68087f56c76
Fredrik Bagge Carlsson <cont-frb@ulund.org>
2016-03-01 12:20:43 +0100

    documentation and fix cost

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using DifferentialDynamicProgramming
 using Base.Test
 
+info("Compile time is high for this package, this is expected and is not an error.")
 # write your own tests here
 demoQP()
 demo_linear()
